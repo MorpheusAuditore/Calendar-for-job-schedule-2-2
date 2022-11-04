83c51ec814989ff963a43e11eb5d9b9c077a7754
Ilnur Zakirov <il_07_29@mail.ru>
2022-11-04 19:15:25 +0300

    add a script for switch months

--- html/JavascriptCode.js
+++ html/JavascriptCode.js
@@ -1,39 +1,51 @@
 ﻿const date = new Date();
-const monthDays = document.querySelector(".days");
-date.setDate(1);
-
-const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-const firstDayIndex = date.getDay() - 1;
-const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
-const nextDay = 7 - lastDayIndex;
-const months = [
-    "Январь",
-    "Февраль",
-    "Март",
-    "Апрель",
-    "Май",
-    "Июнь",
-    "Июль",
-    "Август",
-    "Сентябрь",
-    "Октябрь",
-    "Ноябрь",
-    "Декабрь",
-];
-
-document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-document.querySelector(".date p").innerHTML = date.toDateString();
-let days = "";
-for(let x = firstDayIndex; x>0; x--){
-    days += `<div class = "prevDate">${prevLastDay - x + 1}</div>`;
+function CreateCalendar(){
+    const monthDays = document.querySelector(".days");
+    date.setDate(1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    const firstDayIndex = date.getDay() - 1;
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+    const nextDay = 7 - lastDayIndex;
+    const months = [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+    ];
+    
+    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+    document.querySelector(".date p").innerHTML = new Date().toDateString();
+    let days = "";
+    for(let x = firstDayIndex; x>0; x--){
+        days += `<div class = "prevDate">${prevLastDay - x + 1}</div>`;
+    }
+    
+    for(let i = 1; i <= lastDay; i++){
+        days += `<div>${i}</div>`;
+    }
+    
+    for(let j = 1; j <= nextDay; j++){
+        days += `<div class = "nextDate">${j}</div>`;
+        monthDays.innerHTML = days;
+    }
 }
 
-for(let i = 1; i <= lastDay; i++){
-    days += `<div>${i}</div>`;
-}
+document.querySelector(".prev").addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1);
+    CreateCalendar();
+});
+document.querySelector(".next").addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1);
+    CreateCalendar();
+});
 
-for(let j = 1; j <= nextDay; j++){
-    days += `<div class = "nextDate">${j}</div>`;
-    monthDays.innerHTML = days;
-}
+CreateCalendar();
